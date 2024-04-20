60a4ca854cba483efa5882edab3008f61c33e2c0
Akhin <akhintheruvath2827@gmail.com>
2024-04-20 18:52:38 +0530

    PORT update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const fs = require('fs');
 require('dotenv').config();
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 3000;
 
 const userRouter = require('./routes/user');
 const adminRouter = require('./routes/admin');
